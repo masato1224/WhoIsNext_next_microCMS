83c28a6f8282a2cbfd6987df06dbb3b61a368316
masato1224 <masatomasato1224@gmail.com>
2021-09-04 08:16:40 +0900

    配色変更 gitignoreが機能してない。 codesandboxのバグ？？

--- components/Roulette.tsx
+++ components/Roulette.tsx
@@ -3,7 +3,7 @@ import { Member } from "../interfaces/index";
 import { Wheel, WheelDataType } from "react-custom-roulette";
 import { css } from "linaria";
 import { styled } from "linaria/react";
-import party from 'party-js';
+import party from "party-js";
 
 const styles = {
   root: css`
@@ -18,11 +18,16 @@ const styles = {
     margin-top: 33px !important;
     border-bottom: 0px !important;
     /* from : https://www.ccs1981.jp/blog/css%E3%81%A7%E8%89%B2%E3%82%93%E3%81%AA%E3%82%82%E3%81%AE%E3%82%92%E8%99%B9%E8%89%B2%E3%81%AB%E5%85%89%E3%82%89%E3%81%9B%E3%82%8B/*/
-    background: linear-gradient(to right, Magenta, yellow, Cyan, Magenta) 0% center/200%;
+    background: linear-gradient(to right, Magenta, yellow, Cyan, Magenta) 0%
+      center/200%;
     animation: gaming 0.4s linear infinite;
     @keyframes gaming {
-      0% { background-position-x: 20%; }
-      100% { background-position-x: 200%; }
+      0% {
+        background-position-x: 20%;
+      }
+      100% {
+        background-position-x: 200%;
+      }
     }
   `,
   spinningBack: css`
@@ -34,9 +39,15 @@ const styles = {
     z-index: 0;
     animation: mood 11.35s linear;
     @keyframes mood {
-      6% { background: rgba(0, 0, 0, 0.8); }
-      94% { background: rgba(0, 0, 0, 0.8); }
-      100% { background: rgba(255, 255, 255, 0.0); }
+      6% {
+        background: rgba(0, 0, 0, 0.8);
+      }
+      94% {
+        background: rgba(0, 0, 0, 0.8);
+      }
+      100% {
+        background: rgba(255, 255, 255, 0);
+      }
     }
   `
 };
@@ -57,14 +68,14 @@ const Button = styled.div`
   vertical-align: middle;
   text-decoration: none;
   letter-spacing: 0.1em;
-  color: #FFF;
+  color: #fff;
   border-radius: 0.5rem;
   background-color: #d20010;
   border-bottom: 5px solid #9f000c;
   border-radius: 100vh;
   z-index: 10;
-  &:hover{
-    color: #FFF;
+  &:hover {
+    color: #fff;
     margin-top: 33px;
     border-bottom: 0px solid #9f000c;
     border-radius: 100vh;
@@ -72,17 +83,19 @@ const Button = styled.div`
 `;
 
 const Roulette = ({ members }: { members: Member[] }) => {
-  const [rouletteData, setRouletteData] = useState<WheelDataType[]>([])
-  const [isSpinning, setIsSpinning] = useState<boolean>(false)
+  const [rouletteData, setRouletteData] = useState<WheelDataType[]>([]);
+  const [isSpinning, setIsSpinning] = useState<boolean>(false);
 
-  useEffect(() =>{
-    setRouletteData(members.map((member) => {
-      return {
-        option: member.name,
-        style: { backgroundColor: member.color }
-      }
-    }));
-  }, [members])
+  useEffect(() => {
+    setRouletteData(
+      members.map((member) => {
+        return {
+          option: member.name,
+          style: { backgroundColor: member.color }
+        };
+      })
+    );
+  }, [members]);
 
   return (
     <>
@@ -93,28 +106,28 @@ const Roulette = ({ members }: { members: Member[] }) => {
             setIsSpinning(!isSpinning);
             party.confetti(party.Rect.fromScreen(), {
               count: 1000,
-              shapes: ['square', 'star']
+              shapes: ["square", "star"]
             });
           }}
           prizeNumber={Math.floor(Math.random() * rouletteData.length)}
           data={rouletteData}
           fontSize={20}
           textColors={["#FFF"]}
-          outerBorderColor={'#AAA'}
-          outerBorderWidth={3}
-          radiusLineColor={'#888'}
-          radiusLineWidth={3}
+          outerBorderColor={"#DDD"}
+          outerBorderWidth={5}
+          radiusLineColor={"#fff"}
+          radiusLineWidth={2}
         />
       </div>
       <Button
-        className={isSpinning ? styles.spinning : ''}
+        className={isSpinning ? styles.spinning : ""}
         onClick={() => {
           !isSpinning && setIsSpinning(!isSpinning);
         }}
       >
-       まわれ！！
+        まわれ！！
       </Button>
-      <p className={isSpinning ? styles.spinningBack : ''}></p>
+      <p className={isSpinning ? styles.spinningBack : ""}></p>
     </>
   );
 };
